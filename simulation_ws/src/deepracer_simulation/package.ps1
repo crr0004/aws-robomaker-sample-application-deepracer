c5366ef0a023bf19d018a6bb503841e2e8bccb66
crr0004 <crr0004@gmail.com>
2019-05-19 10:08:47 +1000

    Rename deepracer_simulation_environment to deepracer_simulation
    Remove loading and references to kinesis_video_stream
    Remove loading and references to h264_video_encoding

--- simulation_ws/src/deepracer_simulation/package.ps1
+++ simulation_ws/src/deepracer_simulation/package.ps1
@@ -59,8 +59,8 @@ function colcon_package_source_powershell_script {
 # the prefix is two levels up from the package specific share directory
 $env:COLCON_CURRENT_PREFIX=(Get-Item $PSCommandPath).Directory.Parent.Parent.FullName
 
-colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/deepracer_simulation_environment/hook/cmake_prefix_path.ps1"
-colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/deepracer_simulation_environment/hook/pkg_config.ps1"
-colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/deepracer_simulation_environment/hook/ros_package_path.ps1"
+colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/deepracer_simulation/hook/cmake_prefix_path.ps1"
+colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/deepracer_simulation/hook/pkg_config.ps1"
+colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/deepracer_simulation/hook/ros_package_path.ps1"
 
 Remove-Item Env:\COLCON_CURRENT_PREFIX
--- simulation_ws/src/deepracer_simulation/package.sh
+++ simulation_ws/src/deepracer_simulation/package.sh
@@ -52,7 +52,7 @@ _colcon_prepend_unique_value() {
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_package_sh_COLCON_CURRENT_PREFIX="/opt/workspace/AwsSilverstoneSimulationApplication/build/private/tmp/colcon/install/deepracer_simulation_environment"
+_colcon_package_sh_COLCON_CURRENT_PREFIX="/opt/workspace/AwsSilverstoneSimulationApplication/build/private/tmp/colcon/install/deepracer_simulation"
 if [ -z "$COLCON_CURRENT_PREFIX" ]; then
   if [ ! -d "$_colcon_package_sh_COLCON_CURRENT_PREFIX" ]; then
     echo "The build time path \"$_colcon_package_sh_COLCON_CURRENT_PREFIX\" doesn't exist. Either source a script for a different shell or set the environment variable \"COLCON_CURRENT_PREFIX\" explicitly." 1>&2
@@ -78,9 +78,9 @@ _colcon_package_sh_source_script() {
 }
 
 # source sh hooks
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/deepracer_simulation_environment/hook/cmake_prefix_path.sh"
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/deepracer_simulation_environment/hook/pkg_config.sh"
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/deepracer_simulation_environment/hook/ros_package_path.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/deepracer_simulation/hook/cmake_prefix_path.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/deepracer_simulation/hook/pkg_config.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/deepracer_simulation/hook/ros_package_path.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
